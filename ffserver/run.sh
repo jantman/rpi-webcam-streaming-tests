145b0dbc8f1ba3370db187c4a76b59f2a27d6bd0
Jason Antman <jason@jasonantman.com>
2018-04-29 08:13:22 -0400

    working 1920x1080 30fps mjpeg but constantly logging 'unable to decode APP fields: Invalid data found when processing input' and 'rc buffer underrun'

--- ffserver/run.sh
+++ ffserver/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -f v4l2 -input_format yuyv422 -video_size 1920x1080 -i /dev/video1 -vcodec h264 -f alsa -guess_layout_max 0 -ac 1 -i default:CARD=C615 -c:a vorbis http://127.0.0.1:8090/feed1.ffm
+ffmpeg -f v4l2 -input_format mjpeg -video_size 1920x1080 -i /dev/video1 -vcodec h264 -f alsa -guess_layout_max 0 -ac 1 -i default:CARD=C615 -c:a vorbis http://127.0.0.1:8090/feed1.ffm
