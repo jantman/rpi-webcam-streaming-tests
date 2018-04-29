f8296b72973706a1d8b84933987b75cecb240762
Jason Antman <jason@jasonantman.com>
2018-04-28 21:32:26 -0400

    working on desktop, audio only. good framerate, ~2s delay, but lots of "rc buffer underflow" errors from ffmpeg

--- ffserver/run.sh
+++ ffserver/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -f v4l2 -input_format yuyv422 -video_size 640x480 -i /dev/video0 -vcodec h264_omx http://127.0.0.1:8090/feed1.ffm
+ffmpeg -f v4l2 -input_format yuyv422 -video_size 640x480 -i /dev/video1 -vcodec h264 -f lavfi -i aevalsrc=0 -c:a libmp3lame http://127.0.0.1:8090/feed1.ffm
